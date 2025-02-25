a34b4e5dbe9248cbbe483921b65a9ea3b356edbd
Your Name <your-email@example.com>
2025-02-25 22:38:23 +0300

    Updates

--- assets/extends-DbAM5Q_P.js
+++ assets/extends-DbAM5Q_P.js
@@ -1,4 +1,4 @@
-import{r as Ze,g as mx,j as In}from"./index-DpCUoJdD.js";/**
+import{r as Ze,g as mx,j as In}from"./index-DY_cDt03.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
